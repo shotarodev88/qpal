664222f9d971f6230dc7bf0975b0fb7913a7a6d6
88scripts <122222240+shotarodev88@users.noreply.github.com>
2024-09-22 14:20:03 +0800

    Update DeleteRegistryKey.ps1

--- DeleteRegistryKey.ps1
+++ DeleteRegistryKey.ps1
@@ -1,22 +1,19 @@
-@echo off
+$keyPath = 'HKLM:\SYSTEM\CurrentControlSet\Services\bam\state'
 
-echo Attempting to delete the entire registry key "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\bam\state"...
+echo "Attempting to delete the entire registry key '$keyPath'..."
 
-powershell -Command "Remove-Item 'HKLM:\SYSTEM\CurrentControlSet\Services\bam\state' -Recurse -Force"
+if (Test-Path $keyPath) {
+    Remove-Item $keyPath -Recurse -Force
+    echo "Successfully deleted the 'state' registry key."
+} else {
+    echo "The key does not exist."
+}
 
-if %errorlevel% equ 0 (
-    echo Successfully deleted the "state" registry key.
-) else (
-    echo Failed to delete the "state" registry key or it does not exist.
-)
-
-echo Querying the registry to check if "state" still exists...
-REG QUERY "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\bam\state"
-
-if %errorlevel% equ 0 (
-    echo "state" still exists in the registry.
-) else (
-    echo "state" does not exist in the registry.
-)
+# Query to check if the key still exists
+if (Test-Path $keyPath) {
+    echo "'state' still exists in the registry."
+} else {
+    echo "'state' does not exist in the registry."
+}
 
 pause
